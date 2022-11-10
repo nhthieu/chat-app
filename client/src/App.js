9ce2bab5b73586b02a03abbd97a5b7e399e7a5c8
Hiếu Nguyễn <nguyenhotrunghieu0106@gmail.com>
2022-11-10 16:34:49 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Join from "./components/Join";
+import Chat from "./components/Chat";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Join />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
+  )
+}
